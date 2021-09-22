80fb29e776dc7962232f71aab46a301e2bbeb166
Tamas Pallos <tamas.pallos@vincit.fi>
2021-09-22 15:06:13 +0300

    Add docker build & push CI workflow

---
+++ build-jdbc-urls.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+# we assume here that all secrets are already read to environment variables
+
+# if DB_URL was not set, build it from parts
+if [ -z "${DB_URL+x}" ]; then
+  export DB_PORT=${DB_PORT:-"5432"}
+  export DB_URL="jdbc:postgresql://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}?stringtype=unspecified"
+fi
