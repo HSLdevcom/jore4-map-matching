c669751930d0b672135fc87ef2c096f6862652b9
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-09-24 15:41:22 +0300

    Switch from JTS to Geolatte in jOOQ bindings.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/LineStringBinding.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/LineStringBinding.kt
@@ -1,5 +1,7 @@
 package fi.hsl.jore4.mapmatching.config.jooq.converter
 
+import org.geolatte.geom.C2D
+import org.geolatte.geom.LineString
 import org.jooq.Binding
 import org.jooq.BindingGetResultSetContext
 import org.jooq.BindingGetSQLInputContext
@@ -10,48 +12,47 @@ import org.jooq.BindingSetSQLOutputContext
 import org.jooq.BindingSetStatementContext
 import org.jooq.Converter
 import org.jooq.impl.DSL
-import org.locationtech.jts.geom.LineString
 import java.sql.SQLException
 import java.sql.SQLFeatureNotSupportedException
 import java.sql.Types
 import java.util.Objects
 
-class LineStringBinding : Binding<Any, LineString> {
+class LineStringBinding : Binding<Any, LineString<C2D>> {
 
-    override fun converter(): Converter<Any, LineString> = LineStringConverter.INSTANCE
+    override fun converter(): Converter<Any, LineString<C2D>> = LineStringConverter.INSTANCE
 
     @Throws(SQLException::class)
-    override fun sql(ctx: BindingSQLContext<LineString>) {
+    override fun sql(ctx: BindingSQLContext<LineString<C2D>>) {
         ctx.render().visit(DSL.sql("?::geometry"))
     }
 
     @Throws(SQLException::class)
-    override fun register(ctx: BindingRegisterContext<LineString>) {
+    override fun register(ctx: BindingRegisterContext<LineString<C2D>>) {
         ctx.statement().registerOutParameter(ctx.index(), Types.VARCHAR)
     }
 
     @Throws(SQLException::class)
-    override fun set(ctx: BindingSetStatementContext<LineString>) {
+    override fun set(ctx: BindingSetStatementContext<LineString<C2D>>) {
         ctx.statement().setString(ctx.index(), Objects.toString(ctx.convert(converter()).value(), null))
     }
 
     @Throws(SQLException::class)
-    override fun get(ctx: BindingGetResultSetContext<LineString>) {
+    override fun get(ctx: BindingGetResultSetContext<LineString<C2D>>) {
         ctx.convert(converter()).value(ctx.resultSet().getString(ctx.index()))
     }
 
     @Throws(SQLException::class)
-    override fun get(ctx: BindingGetStatementContext<LineString>) {
+    override fun get(ctx: BindingGetStatementContext<LineString<C2D>>) {
         ctx.convert(converter()).value(ctx.statement().getString(ctx.index()))
     }
 
     @Throws(SQLException::class)
-    override fun set(ctx: BindingSetSQLOutputContext<LineString>) {
+    override fun set(ctx: BindingSetSQLOutputContext<LineString<C2D>>) {
         throw SQLFeatureNotSupportedException()
     }
 
     @Throws(SQLException::class)
-    override fun get(ctx: BindingGetSQLInputContext<LineString>) {
+    override fun get(ctx: BindingGetSQLInputContext<LineString<C2D>>) {
         throw SQLFeatureNotSupportedException()
     }
 }
--- spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/GeometryConverterTest.kt
+++ spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/GeometryConverterTest.kt
@@ -1,84 +1,84 @@
 package fi.hsl.jore4.mapmatching.config.jooq.converter
 
+import org.geolatte.geom.C2D
+import org.geolatte.geom.Geometry
+import org.geolatte.geom.GeometryType
+import org.geolatte.geom.LineString
+import org.geolatte.geom.Point
+import org.geolatte.geom.builder.DSL.c
+import org.geolatte.geom.builder.DSL.linestring
+import org.geolatte.geom.builder.DSL.point
+import org.geolatte.geom.crs.CrsRegistry
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.notNullValue
 import org.hamcrest.Matchers.nullValue
 import org.junit.jupiter.api.Test
-import org.locationtech.jts.geom.Coordinate
-import org.locationtech.jts.geom.Geometry
-import org.locationtech.jts.geom.GeometryFactory
-import org.locationtech.jts.geom.LineString
-import org.locationtech.jts.geom.Point
-import org.locationtech.jts.geom.PrecisionModel
 import kotlin.test.assertFailsWith
 
 class GeometryConverterTest {
 
     companion object {
-        private const val EWKB_POINT = "01010000A0E6100000E9254A6BA1154E404A9E1B3507F338400000000000000000"
-        private const val EWKB_LINESTRING =
-            "01020000A0E610000002000000E9254A6BA1154E404A9E1B3507F338400000000000000000BE9150659E154E40103A46840FF238400000000000000000"
-    }
+        private val EPSG_3067 = CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(3067)
 
-    private val geomFactory = GeometryFactory(PrecisionModel(), 4326)
+        private const val EWKB_POINT = "0101000020FB0B0000666666660C8C1741CDCCCC4CCE735941"
+        private const val EWKB_LINESTRING =
+            "0102000020FB0B000002000000666666660C8C1741CDCCCC4CCE73594100000000B0931741000000C0B9745941"
+    }
 
     @Test
     fun testFrom_withPoint_whenTypeNameIsSetAsPoint() {
-        val point: Geometry? = GeometryConverter(Geometry.TYPENAME_POINT).from(EWKB_POINT)
+        val point: Geometry<C2D>? = GeometryConverter(GeometryType.POINT).from(EWKB_POINT)
 
         assertThat(point, `is`(notNullValue()))
-        assertThat(point, equalTo(geomFactory.createPoint(Coordinate(60.16898862, 24.949328727))))
+        assertThat(point, equalTo(point(EPSG_3067, c(385795.1, 6672185.2))))
     }
 
     @Test
     fun testFrom_withPoint_whenTypeNameIsSetAsLineString() {
         val exception = assertFailsWith<IllegalArgumentException> {
-            GeometryConverter(Geometry.TYPENAME_LINESTRING).from(EWKB_POINT)
+            GeometryConverter(GeometryType.LINESTRING).from(EWKB_POINT)
         }
         assertThat(exception.message, equalTo("Unsupported geometry type: Point"))
     }
 
     @Test
     fun testFrom_withLineString_whenTypeNameIsSetAsLineString() {
-        val lineString: Geometry? = GeometryConverter(Geometry.TYPENAME_LINESTRING).from(EWKB_LINESTRING)
+        val lineString: Geometry<C2D>? = GeometryConverter(GeometryType.LINESTRING).from(EWKB_LINESTRING)
 
         assertThat(lineString, `is`(notNullValue()))
         assertThat(lineString,
-                   equalTo(geomFactory.createLineString(arrayOf(Coordinate(60.168988620, 24.949328727),
-                                                                Coordinate(60.168896355, 24.945549266)))))
+                   equalTo(linestring(EPSG_3067, c(385795.1, 6672185.2), c(386284.0, 6673127.0))))
     }
 
     @Test
     fun testFrom_withLineString_whenTypeNameIsSetAsPoint() {
         val exception = assertFailsWith<IllegalArgumentException> {
-            GeometryConverter(Geometry.TYPENAME_POINT).from(EWKB_LINESTRING)
+            GeometryConverter(GeometryType.POINT).from(EWKB_LINESTRING)
         }
         assertThat(exception.message, equalTo("Unsupported geometry type: LineString"))
     }
 
     @Test
     fun testFrom_withNull() {
-        assertThat(GeometryConverter(Geometry.TYPENAME_POINT).from(null), `is`(nullValue()))
-        assertThat(GeometryConverter(Geometry.TYPENAME_LINESTRING).from(null), `is`(nullValue()))
+        assertThat(GeometryConverter(GeometryType.POINT).from(null), `is`(nullValue()))
+        assertThat(GeometryConverter(GeometryType.LINESTRING).from(null), `is`(nullValue()))
     }
 
     @Test
     fun testTo_withPoint() {
-        val point: Point = geomFactory.createPoint(Coordinate(24.123, 60.456))
+        val point: Point<C2D> = point(EPSG_3067, c(385795.1, 6672185.2))
 
-        assertThat(GeometryConverter.to(point), equalTo("SRID=4326;POINT (24.123 60.456)"))
+        assertThat(GeometryConverter.to(point), equalTo("SRID=3067;POINT(385795.1 6672185.2)"))
     }
 
     @Test
     fun testTo_withLineString() {
-        val lineString: LineString =
-            geomFactory.createLineString(arrayOf(Coordinate(24.123, 60.234), Coordinate(24.345, 60.456)))
+        val lineString: LineString<C2D> = linestring(EPSG_3067, c(385795.1, 6672185.2), c(386284.0, 6673127.0))
 
-        assertThat(
-            GeometryConverter.to(lineString),
-            equalTo("SRID=4326;LINESTRING (24.123 60.234, 24.345 60.456)"))
+        assertThat(GeometryConverter.to(lineString),
+                   equalTo("SRID=3067;LINESTRING(385795.1 6672185.2,386284 6673127)"))
     }
 
     @Test
