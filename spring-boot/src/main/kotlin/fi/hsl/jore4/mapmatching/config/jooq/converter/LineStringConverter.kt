8861dd88af2d2f87141098f2e598f1e20832db46
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-09-24 15:41:22 +0300

    Switch from JTS to Geolatte in jOOQ bindings.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/LineStringConverter.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/jooq/converter/LineStringConverter.kt
@@ -1,25 +1,26 @@
 package fi.hsl.jore4.mapmatching.config.jooq.converter
 
+import org.geolatte.geom.C2D
+import org.geolatte.geom.GeometryType
+import org.geolatte.geom.LineString
 import org.jooq.Converter
-import org.locationtech.jts.geom.Geometry
-import org.locationtech.jts.geom.LineString
 
 /**
  * Converts between LineString geometries and PostGIS EWKT/EWKB formats.
  */
-class LineStringConverter : Converter<Any, LineString> {
+class LineStringConverter : Converter<Any, LineString<C2D>> {
 
     override fun from(databaseObject: Any?) = CONVERTER.from(databaseObject) as LineString?
 
-    override fun to(userObject: LineString): Any? = GeometryConverter.to(userObject)
+    override fun to(userObject: LineString<C2D>): Any? = GeometryConverter.to(userObject)
 
     override fun fromType() = Any::class.java
 
-    override fun toType() = LineString::class.java
+    override fun toType() = LineString::class.java as Class<LineString<C2D>>
 
     companion object {
         val INSTANCE = LineStringConverter()
 
-        private val CONVERTER = GeometryConverter(Geometry.TYPENAME_LINESTRING)
+        private val CONVERTER = GeometryConverter(GeometryType.LINESTRING)
     }
 }
