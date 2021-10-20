b799f7ef201dd5e5f25fd56b087031721a7c462f
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-10-20 11:40:41 +0300

    Add road/street names into routing result.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/model/PathTraversal.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/model/PathTraversal.kt
@@ -1,5 +1,6 @@
 package fi.hsl.jore4.mapmatching.model
 
+import fi.hsl.jore4.mapmatching.util.MultilingualString
 import org.geolatte.geom.G2D
 import org.geolatte.geom.LineString
 
@@ -7,4 +8,5 @@ data class PathTraversal(val infrastructureLinkId: Long,
                          val externalLinkRef: ExternalLinkReference,
                          val alongLinkDirection: Boolean, // along or against the direction of link geometry
                          val cost: Double,  // cost is currently the same as the 3D length of the link geometry
+                         val infrastructureLinkName: MultilingualString,
                          val geom: LineString<G2D>)
---
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/util/component/JsonbConverterImpl.kt
@@ -0,0 +1,27 @@
+package fi.hsl.jore4.mapmatching.util.component
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.jooq.JSONB
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class JsonbConverterImpl @Autowired constructor(val objectMapper: ObjectMapper) : IJsonbConverter {
+
+    override fun asJson(obj: Any?): JSONB {
+        return try {
+            JSONB.jsonb(objectMapper.writeValueAsString(obj))
+        } catch (e: JsonProcessingException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    override fun <T> fromJson(json: JSONB, clazz: Class<T>): T {
+        return try {
+            objectMapper.readValue(json.data(), clazz)
+        } catch (e: JsonProcessingException) {
+            throw RuntimeException(e)
+        }
+    }
+}
