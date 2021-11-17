ccaebe2b7cc628ca3108f9a9bf124f4c3b2a796e
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-11-17 19:12:53 +0200

    Move CORS header settings under `development` profile.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebConfig.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package fi.hsl.jore4.mapmatching.config
 
 import fi.hsl.jore4.mapmatching.api.RouteController
+import fi.hsl.jore4.mapmatching.config.profile.Development
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
@@ -10,10 +11,16 @@ import org.springframework.web.util.UrlPathHelper
 @Configuration
 class WebConfig : WebMvcConfigurer {
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping(RouteController.URL_PREFIX + "/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET")
+    @Development
+    @Configuration
+    class DevelopmentWebConfig : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            // To fix browser errors while trying to invoke routing API from
+            // a web page served from a local disk.
+            registry.addMapping(RouteController.URL_PREFIX + "/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET")
+        }
     }
 
     override fun configurePathMatch(pathMatchConfig: PathMatchConfigurer) {
---
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/profile/Production.kt
@@ -0,0 +1,8 @@
+package fi.hsl.jore4.mapmatching.config.profile
+
+import org.springframework.context.annotation.Profile
+
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
+@Retention
+@Profile(SpringProfiles.PRODUCTION)
+annotation class Production()
