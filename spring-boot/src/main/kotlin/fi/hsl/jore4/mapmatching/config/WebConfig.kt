dca303b9b823c45d9fb8c2b44e909c8d65f85c88
Jarkko Kaura <jarkko.kaura@vincit.fi>
2021-11-23 17:00:24 +0200

    Add static landing page for easy testing.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebConfig.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebConfig.kt
@@ -4,7 +4,10 @@ import fi.hsl.jore4.mapmatching.api.RouteController
 import fi.hsl.jore4.mapmatching.config.profile.Development
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.resource.EncodedResourceResolver
+import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
@@ -13,11 +16,21 @@ class WebConfig : WebMvcConfigurer {
     @Configuration
     class DevelopmentWebConfig : WebMvcConfigurer {
         override fun addCorsMappings(registry: CorsRegistry) {
-            // To fix browser errors while trying to invoke routing API from
+            // To fix browser errors while trying to invoke route API from
             // a web page served from a local disk.
             registry.addMapping(RouteController.URL_PREFIX + "/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET")
         }
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+            .addResourceHandler("/*")
+            .addResourceLocations("classpath:/static/")
+            .setCachePeriod(3600)
+            .resourceChain(true)
+            .addResolver(EncodedResourceResolver())
+            .addResolver(PathResourceResolver())
     }
 }
