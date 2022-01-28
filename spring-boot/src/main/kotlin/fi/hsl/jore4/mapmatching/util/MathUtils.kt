15013ed2b11001a38fe6ca1f89b3d0ea16b5690f
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-01-28 20:42:11 +0200

    Do minor code improvements.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/util/MathUtils.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/util/MathUtils.kt
@@ -1,6 +1,5 @@
 package fi.hsl.jore4.mapmatching.util
 
-import java.math.BigDecimal
 import kotlin.math.abs
 
 @Suppress("MemberVisibilityCanBePrivate")
@@ -8,7 +7,7 @@ object MathUtils {
 
     const val ZERO_THRESHOLD: Double = 0.00001
 
-    fun compare(d1: Double, d2: Double): Int = BigDecimal(d1).compareTo(BigDecimal(d2))
+    fun compare(d1: Double, d2: Double): Int = d1.compareTo(d2)
 
     fun isZero(d: Double): Boolean = abs(d) < ZERO_THRESHOLD
 }
