41d4d76387d1aceb84d524b025c47bf233c6bb99
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-02-23 22:33:40 +0200

    Do renaming in `SnappedLinkStateGenerator` test class.

--- spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/test/generators/SnappedLinkStateGenerator.kt
+++ spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/test/generators/SnappedLinkStateGenerator.kt
@@ -37,7 +37,7 @@ object SnappedLinkStateGenerator {
 
     private val DISTANCE_PAIR: Gen<Pair<Double, Double>> = shuffledPair(NON_NEGATIVE_DISTANCE, POSITIVE_DISTANCE)
 
-    private val SNAP_FRACTIONAL: Gen<Double> = doubles().between(0.0, 1.0)
+    private val SNAP_POINT_LOCATION_FRACTION: Gen<Double> = doubles().between(0.0, 1.0)
 
     private val LINK_LENGTH: Gen<Double> = doubles().between(2.0, 5_000.0)
 
@@ -75,14 +75,17 @@ object SnappedLinkStateGenerator {
     }
 
     fun snapSingleLinkTwice(withDiscreteEndpoints: Boolean? = null): Gen<Pair<SnappedLinkState, SnappedLinkState>> {
-        val numberOfDiscreteNodesGen: Gen<Int> = when (withDiscreteEndpoints) {
+        val genNumberOfDiscreteNodes: Gen<Int> = when (withDiscreteEndpoints) {
             true -> constant(2)
             false -> constant(1)
             else -> integers().between(1, 2)
         }
 
         return snapTwoLinks(duplicate(infrastructureLinkId()),
-                            numberOfDiscreteNodesGen.flatMap { nodeIdQuadruple(it, discreteNodesBetweenHalves = false) })
+                            genNumberOfDiscreteNodes.flatMap {
+                                nodeIdQuadruple(it,
+                                                discreteNodesBetweenHalves = false)
+                            })
     }
 
     // Generate pairs of links having a common node.
@@ -110,57 +113,65 @@ object SnappedLinkStateGenerator {
         require(numberOfDiscreteNodes in 1..4) {
             "numberOfDiscreteNodes must be in range 1..4, but was: $numberOfDiscreteNodes"
         }
+
         return snapTwoLinks(infrastructureLinkIdPair(),
                             nodeIdQuadruple(numberOfDiscreteNodes, discreteNodesBetweenHalves = true))
     }
 
-    fun snapTwoLinks(linkIdsGen: Gen<Pair<InfrastructureLinkId, InfrastructureLinkId>>,
-                     nodeIdsGen: Gen<Quadruple<InfrastructureNodeId, InfrastructureNodeId, InfrastructureNodeId, InfrastructureNodeId>>)
+    private fun snapTwoLinks(genLinkIdPair: Gen<Pair<InfrastructureLinkId, InfrastructureLinkId>>,
+                             genNodeIds: Gen<Quadruple<InfrastructureNodeId, InfrastructureNodeId, InfrastructureNodeId, InfrastructureNodeId>>)
         : Gen<Pair<SnappedLinkState, SnappedLinkState>> {
 
-        return linkIdsGen.zip(DISTANCE_PAIR,
-                              pair(SNAP_FRACTIONAL),
-                              pair(LINK_LENGTH),
-                              nodeIdsGen) { (linkId1, linkId2),
-                                            (distanceToLink1, distanceToLink2),
-                                            (snapFractional1, snapFractional2),
-                                            (linkLength1, linkLength2),
-                                            (nodeId1, nodeId2, nodeId3, nodeId4) ->
+        return genLinkIdPair.zip(DISTANCE_PAIR,
+                                 pair(SNAP_POINT_LOCATION_FRACTION),
+                                 pair(LINK_LENGTH),
+                                 genNodeIds) { (linkId1, linkId2),
+                                               (distanceToLink1, distanceToLink2),
+                                               (snapPointLocationFraction1, snapPointLocationFraction2),
+                                               (linkLength1, linkLength2),
+                                               (nodeId1, nodeId2, nodeId3, nodeId4) ->
 
-            Pair(createSnappedLinkState(linkId1, distanceToLink1, snapFractional1, linkLength1, nodeId1, nodeId2),
-                 createSnappedLinkState(linkId2, distanceToLink2, snapFractional2, linkLength2, nodeId3, nodeId4))
+            Pair(createSnappedLinkState(linkId1,
+                                        distanceToLink1,
+                                        snapPointLocationFraction1,
+                                        linkLength1,
+                                        nodeId1, nodeId2),
+                 createSnappedLinkState(linkId2,
+                                        distanceToLink2,
+                                        snapPointLocationFraction2,
+                                        linkLength2,
+                                        nodeId3, nodeId4))
         }
     }
 
     private fun generateSnappedLinkStateFromOneNode(nodeProximityFilter: LinkEndpointsProximityFilter): Gen<SnappedLinkState> =
-        generateSnappedLinkState(generateSnapFractional(nodeProximityFilter), nodeIdPair(false))
+        generateSnappedLinkState(generateSnapPointLocationFraction(nodeProximityFilter), nodeIdPair(false))
 
     private fun generateSnappedLinkStateFromTwoNodes(nodeProximityFilter: LinkEndpointsProximityFilter): Gen<SnappedLinkState> =
-        generateSnappedLinkState(generateSnapFractional(nodeProximityFilter), discreteNodeIdPair())
+        generateSnappedLinkState(generateSnapPointLocationFraction(nodeProximityFilter), discreteNodeIdPair())
 
-    private fun generateSnappedLinkState(snapFractionalGen: Gen<Double>,
-                                         nodeIdPairGen: Gen<Pair<InfrastructureNodeId, InfrastructureNodeId>>)
+    private fun generateSnappedLinkState(genSnapPointLocationFraction: Gen<Double>,
+                                         genNodeIdPair: Gen<Pair<InfrastructureNodeId, InfrastructureNodeId>>)
         : Gen<SnappedLinkState> {
 
         return infrastructureLinkId().zip(NON_NEGATIVE_DISTANCE,
-                                          snapFractionalGen,
+                                          genSnapPointLocationFraction,
                                           LINK_LENGTH,
-                                          nodeIdPairGen) { infrastructureLinkId,
+                                          genNodeIdPair) { infrastructureLinkId,
                                                            distanceToLink,
-                                                           snapFractional,
+                                                           snapPointLocationFraction,
                                                            linkLength,
                                                            (startNodeId, endNodeId) ->
 
             createSnappedLinkState(infrastructureLinkId,
                                    distanceToLink,
-                                   snapFractional,
+                                   snapPointLocationFraction,
                                    linkLength,
-                                   startNodeId,
-                                   endNodeId)
+                                   startNodeId, endNodeId)
         }
     }
 
-    private fun generateSnapFractional(nodeProximityFilter: LinkEndpointsProximityFilter): Gen<Double> =
+    private fun generateSnapPointLocationFraction(nodeProximityFilter: LinkEndpointsProximityFilter): Gen<Double> =
         when (nodeProximityFilter) {
             START_NODE_CLOSER -> doubles().between(0.0, 0.5 - DOUBLE_TOLERANCE)
             END_NODE_CLOSER -> doubles().between(0.5 + DOUBLE_TOLERANCE, 1.0)
@@ -171,22 +182,21 @@ object SnappedLinkStateGenerator {
 
     private fun createSnappedLinkState(infrastructureLinkId: InfrastructureLinkId,
                                        distanceToLink: Double,
-                                       snapPointFractional: Double,
+                                       snapPointLocationFraction: Double,
                                        linkLength: Double,
                                        startNodeId: InfrastructureNodeId,
                                        endNodeId: InfrastructureNodeId)
         : SnappedLinkState {
 
         val snappedToEndpointNode: Boolean =
-            snapPointFractional.isWithinTolerance(0.0) || snapPointFractional.isWithinTolerance(1.0)
+            snapPointLocationFraction.isWithinTolerance(0.0) || snapPointLocationFraction.isWithinTolerance(1.0)
 
         val closestDistanceToLink: Double = if (snappedToEndpointNode) 0.0 else distanceToLink
 
         return SnappedLinkState(infrastructureLinkId,
                                 closestDistanceToLink,
-                                snapPointFractional,
+                                snapPointLocationFraction,
                                 linkLength,
-                                startNodeId,
-                                endNodeId)
+                                startNodeId, endNodeId)
     }
 }
