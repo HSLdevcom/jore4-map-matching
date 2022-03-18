413d3fff162980474562b222a5b121c4c43c8bd2
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-03-18 17:03:51 +0200

    Add local cache for long queries generated in `StopRepositoryImpl`.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/repository/infrastructure/StopRepositoryImpl.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/repository/infrastructure/StopRepositoryImpl.kt
@@ -11,9 +11,12 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.sql.ResultSet
+import java.util.concurrent.ConcurrentHashMap
 
 @Repository
 class StopRepositoryImpl @Autowired constructor(val jdbcTemplate: NamedParameterJdbcTemplate) : IStopRepository {
+
+    private val cachedQueriesForFindingStopsByNationalId = ConcurrentHashMap<Int, String>()
 
     @Transactional(readOnly = true)
     override fun findStopsAndSnapToInfrastructureLinks(stopMatchParams: Collection<PublicTransportStopMatchParameters>,
@@ -24,7 +27,7 @@ class StopRepositoryImpl @Autowired constructor(val jdbcTemplate: NamedParameter
             return emptyList()
         }
 
-        val queryString: String = createQueryForFindingStopsByNationalId(stopMatchParams.size)
+        val queryString: String = getQueryForFindingStopsByNationalId(stopMatchParams.size)
 
         val jdbcParams = MapSqlParameterSource()
             .addValue("vehicleType", VehicleType.GENERIC_BUS.value)
@@ -61,6 +64,10 @@ class StopRepositoryImpl @Autowired constructor(val jdbcTemplate: NamedParameter
         }
     }
 
+    private fun getQueryForFindingStopsByNationalId(numberOfStops: Int): String =
+        cachedQueriesForFindingStopsByNationalId.computeIfAbsent(numberOfStops,
+                                                                 StopRepositoryImpl::createQueryForFindingStopsByNationalId)
+
     companion object {
 
         private fun createQueryForFindingStopsByNationalId(numberOfStops: Int): String {
@@ -72,30 +79,30 @@ class StopRepositoryImpl @Autowired constructor(val jdbcTemplate: NamedParameter
                 },
                 // Indent in separator should match the main query string below in order to have
                 // properly formatted SQL result.
-                separator = "\n                UNION "
+                separator = "\n                    UNION "
             )
 
             return """
-            SELECT
-                stop_params.stop_national_id,
-                link.infrastructure_link_id,
-                link.start_node_id,
-                link.end_node_id,
-                link.traffic_flow_direction_type,
-                ST_LineLocatePoint(link.geom, stop.geom) AS fractional_measure,
-                ST_Length(link.geom) AS infrastructure_link_len2d
-            FROM (
-                ${createUnionSubquery(numberOfStops)}
-            ) stop_params
-            INNER JOIN routing.public_transport_stop stop ON
-                stop.public_transport_stop_national_id = stop_params.stop_national_id
-            INNER JOIN routing.infrastructure_link link ON
-                link.infrastructure_link_id = stop.located_on_infrastructure_link_id
-            INNER JOIN routing.infrastructure_link_safely_traversed_by_vehicle_type safe
-                ON safe.infrastructure_link_id = link.infrastructure_link_id
-            WHERE ST_DWithin(stop.geom, ST_Transform(ST_GeomFromEWKB(stop_params.src_location), 3067), :maxDistance)
-                AND safe.vehicle_type = :vehicleType
-            ORDER BY stop_params.seq ASC;""".trimIndent()
+                SELECT
+                    stop_params.stop_national_id,
+                    link.infrastructure_link_id,
+                    link.start_node_id,
+                    link.end_node_id,
+                    link.traffic_flow_direction_type,
+                    ST_LineLocatePoint(link.geom, stop.geom) AS fractional_measure,
+                    ST_Length(link.geom) AS infrastructure_link_len2d
+                FROM (
+                    ${createUnionSubquery(numberOfStops)}
+                ) stop_params
+                INNER JOIN routing.public_transport_stop stop ON
+                    stop.public_transport_stop_national_id = stop_params.stop_national_id
+                INNER JOIN routing.infrastructure_link link ON
+                    link.infrastructure_link_id = stop.located_on_infrastructure_link_id
+                INNER JOIN routing.infrastructure_link_safely_traversed_by_vehicle_type safe
+                    ON safe.infrastructure_link_id = link.infrastructure_link_id
+                WHERE ST_DWithin(stop.geom, ST_Transform(ST_GeomFromEWKB(stop_params.src_location), 3067), :maxDistance)
+                    AND safe.vehicle_type = :vehicleType
+                ORDER BY stop_params.seq ASC;""".trimIndent()
         }
     }
 }
