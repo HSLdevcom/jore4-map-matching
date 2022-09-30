f4f005e4e4f82296d8bc73a357b47dec2fc4b9d2
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-09-30 13:43:03 +0300

    Fix fragility of decimal precision of floating point numbers in integration tests.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/util/GeolatteUtils.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/util/GeolatteUtils.kt
@@ -13,6 +13,9 @@ import org.geolatte.geom.Point
 import org.geolatte.geom.PositionSequenceBuilders
 import org.geolatte.geom.codec.Wkb
 import org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84
+import java.math.BigDecimal
+import java.math.RoundingMode.HALF_UP
+import kotlin.math.max
 
 object GeolatteUtils {
 
@@ -59,6 +62,23 @@ object GeolatteUtils {
             line.positions.drop(1).forEach(positionSequenceBuilder::add)
 
             prevLineLastPosition = line.endPosition
+        }
+
+        return mkLineString(positionSequenceBuilder.toPositionSequence(), WGS84)
+    }
+
+    fun roundCoordinates(line: LineString<G2D>, decimalPrecision: Int): LineString<G2D> {
+        val positionSequenceBuilder = PositionSequenceBuilders.variableSized(G2D::class.java)
+
+        fun round(num: Double): Double {
+            val bd = BigDecimal.valueOf(num)
+            val numDecimals: Int = max(0, bd.stripTrailingZeros().scale())
+
+            return if (numDecimals <= decimalPrecision) num else bd.setScale(decimalPrecision, HALF_UP).toDouble()
+        }
+
+        line.positions.forEach { pos: G2D ->
+            positionSequenceBuilder.add(G2D(round(pos.lon), round(pos.lat)))
         }
 
         return mkLineString(positionSequenceBuilder.toPositionSequence(), WGS84)
--- spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/service/routing/RoutingService_FindRouteTest.kt
+++ spring-boot/src/test/kotlin/fi/hsl/jore4/mapmatching/service/routing/RoutingService_FindRouteTest.kt
@@ -6,6 +6,7 @@ import fi.hsl.jore4.mapmatching.service.common.response.ResponseCode
 import fi.hsl.jore4.mapmatching.service.common.response.RoutingResponse
 import fi.hsl.jore4.mapmatching.test.IntTest
 import fi.hsl.jore4.mapmatching.test.IntegrationTest
+import fi.hsl.jore4.mapmatching.util.GeolatteUtils.roundCoordinates
 import fi.hsl.jore4.mapmatching.util.GeolatteUtils.toPoint
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
@@ -131,33 +132,33 @@ class RoutingService_FindRouteTest @Autowired constructor(val routingService: IR
     @DisplayName("When result route via request points span multiple infrastructure links")
     inner class WhenResultRouteViaRequestPointsSpanMultipleInfrastructureLinks {
 
-        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95707730, 60.16800990)),
-                                                                  toPoint(g(24.95851405, 60.16911157)))
+        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95708, 60.16801)),
+                                                                  toPoint(g(24.95851, 60.16911)))
 
         @Test
         fun shouldReturnExpectedGeometry() {
             findRouteAndCheckAssertionsOnSuccessResponse(requestRoutePoints) { resp ->
 
-                val positionSequenceBuilder = PositionSequenceBuilders.variableSized(G2D::class.java)
-                    .add(24.957078115997536, 60.168008946494965)
-                    .add(24.95715147812108, 60.16802453476507)
-                    .add(24.95723875994145, 60.168060496789664)
-                    .add(24.957323462359152, 60.168123777899666)
-                    .add(24.95758037487319, 60.16831753532955)
-                    .add(24.957757980575668, 60.168433705609274)
-                    .add(24.957965939306, 60.168577522712035)
-                    .add(24.958263473250017, 60.16876486296617)
-                    .add(24.958394690444972, 60.168845216182085)
-                    .add(24.95847818526909, 60.16892453755149)
-                    .add(24.958509823912653, 60.16906042733862)
-                    .add(24.958509747716203, 60.16911156840859)
+                val expectedCoordinates = PositionSequenceBuilders.variableSized(G2D::class.java)
+                    .add(24.95708, 60.16801)
+                    .add(24.95715, 60.16802)
+                    .add(24.95724, 60.16806)
+                    .add(24.95732, 60.16812)
+                    .add(24.95758, 60.16832)
+                    .add(24.95776, 60.16843)
+                    .add(24.95797, 60.16858)
+                    .add(24.95826, 60.16876)
+                    .add(24.95839, 60.16885)
+                    .add(24.95848, 60.16892)
+                    .add(24.95851, 60.16906)
+                    .add(24.95851, 60.16911)
 
                 val expectedGeometry: LineString<G2D> =
-                    mkLineString(positionSequenceBuilder.toPositionSequence(), WGS84)
+                    mkLineString(expectedCoordinates.toPositionSequence(), WGS84)
 
                 val actualGeometry: LineString<G2D> = resp.routes.first().geometry
 
-                assertThat(actualGeometry).isEqualTo(expectedGeometry)
+                assertThat(roundCoordinates(actualGeometry, 5)).isEqualTo(expectedGeometry)
             }
         }
 
@@ -184,24 +185,24 @@ class RoutingService_FindRouteTest @Autowired constructor(val routingService: IR
     @DisplayName("When request route points coincide single one-way link in the forward direction")
     inner class WhenRequestRoutePointsCoincideSingleOneWayLinkInForwardDirection {
 
-        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95734619, 60.16813263)),
-                                                                  toPoint(g(24.95762143, 60.16834154)))
+        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95735, 60.16813)),
+                                                                  toPoint(g(24.95762, 60.16834)))
 
         @Test
         fun shouldReturnExpectedGeometry() {
             findRouteAndCheckAssertionsOnSuccessResponse(requestRoutePoints) { resp ->
 
-                val positionSequenceBuilder = PositionSequenceBuilders.variableSized(G2D::class.java)
-                    .add(24.957338539378313, 60.16813514871233)
-                    .add(24.95758037487319, 60.16831753532955)
-                    .add(24.957618673627945, 60.168342586286755)
+                val expectedCoordinates = PositionSequenceBuilders.variableSized(G2D::class.java)
+                    .add(24.95734, 60.16813)
+                    .add(24.95758, 60.16832)
+                    .add(24.95762, 60.16834)
 
                 val expectedGeometry: LineString<G2D> =
-                    mkLineString(positionSequenceBuilder.toPositionSequence(), WGS84)
+                    mkLineString(expectedCoordinates.toPositionSequence(), WGS84)
 
                 val actualGeometry: LineString<G2D> = resp.routes.first().geometry
 
-                assertThat(actualGeometry).isEqualTo(expectedGeometry)
+                assertThat(roundCoordinates(actualGeometry, 5)).isEqualTo(expectedGeometry)
             }
         }
 
@@ -226,31 +227,31 @@ class RoutingService_FindRouteTest @Autowired constructor(val routingService: IR
     @DisplayName("When request route points coincide single one-way link in the backward direction")
     inner class WhenRequestRoutePointsCoincideSingleOneWayLinkInBackwardDirection {
 
-        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95762143, 60.16834154)),
-                                                                  toPoint(g(24.95734619, 60.16813263)))
+        private val requestRoutePoints: List<Point<G2D>> = listOf(toPoint(g(24.95762, 60.16834)),
+                                                                  toPoint(g(24.95735, 60.16813)))
 
         @Test
         fun shouldReturnExpectedGeometry() {
             findRouteAndCheckAssertionsOnSuccessResponse(requestRoutePoints) { resp ->
 
-                val positionSequenceBuilder = PositionSequenceBuilders.variableSized(G2D::class.java)
-                    .add(24.957618673627945, 60.168342586286755)
-                    .add(24.957757980575668, 60.168433705609274)
-                    .add(24.957632827286027, 60.1684575186299)
-                    .add(24.957461812613946, 60.168342617436046)
-                    .add(24.957336775866754, 60.16824669407173)
-                    .add(24.957175919885138, 60.16813717657471)
-                    .add(24.957109340744697, 60.16810512648815)
-                    .add(24.95723875994145, 60.168060496789664)
-                    .add(24.957323462359152, 60.168123777899666)
-                    .add(24.957338539378313, 60.16813514871233)
+                val expectedCoordinates = PositionSequenceBuilders.variableSized(G2D::class.java)
+                    .add(24.95762, 60.16834)
+                    .add(24.95776, 60.16843)
+                    .add(24.95763, 60.16846)
+                    .add(24.95746, 60.16834)
+                    .add(24.95734, 60.16825)
+                    .add(24.95718, 60.16814)
+                    .add(24.95711, 60.16811)
+                    .add(24.95724, 60.16806)
+                    .add(24.95732, 60.16812)
+                    .add(24.95734, 60.16813)
 
                 val expectedGeometry: LineString<G2D> =
-                    mkLineString(positionSequenceBuilder.toPositionSequence(), WGS84)
+                    mkLineString(expectedCoordinates.toPositionSequence(), WGS84)
 
                 val actualGeometry: LineString<G2D> = resp.routes.first().geometry
 
-                assertThat(actualGeometry).isEqualTo(expectedGeometry)
+                assertThat(roundCoordinates(actualGeometry, 5)).isEqualTo(expectedGeometry)
             }
         }
 
