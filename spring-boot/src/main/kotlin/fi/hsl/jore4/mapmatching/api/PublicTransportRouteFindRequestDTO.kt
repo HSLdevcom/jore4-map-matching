e7320f060410216c764944fa937c149285187bd8
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-10-14 10:41:48 +0300

    Fix incorrect comment section in `PublicTransportRouteFindRequestDTO`.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/api/PublicTransportRouteFindRequestDTO.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/api/PublicTransportRouteFindRequestDTO.kt
@@ -4,13 +4,11 @@ import fi.hsl.jore4.mapmatching.model.LatLng
 import javax.validation.Valid
 
 /**
- * Contains input data for map-matching a public transport route against the
+ * Contains input data for finding a route via given points through the
  * infrastructure network provided by the system.
  *
- * @property routeCoordinates route points of the route
+ * @property routePoints route points of the route
  * @property linkSearchRadius optional parameter for the link search radius
  */
 data class PublicTransportRouteFindRequestDTO(@field:Valid val routePoints: List<LatLng>,
-                                              val linkSearchRadius: Int?) {
-
-}
+                                              val linkSearchRadius: Int?)
