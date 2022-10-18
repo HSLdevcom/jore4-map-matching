c1292c691a5d61436c60d802a8d96dca06fe6dc5
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-10-18 18:12:07 +0300

    Extract road junction matching to a separate (internal) service.

---
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/service/matching/RoadJunctionMatcherImpl.kt
@@ -0,0 +1,78 @@
+package fi.hsl.jore4.mapmatching.service.matching
+
+import fi.hsl.jore4.mapmatching.model.NodeProximity
+import fi.hsl.jore4.mapmatching.model.VehicleType
+import fi.hsl.jore4.mapmatching.model.matching.RouteJunctionPoint
+import fi.hsl.jore4.mapmatching.model.matching.RoutePoint
+import fi.hsl.jore4.mapmatching.repository.routing.INodeRepository
+import fi.hsl.jore4.mapmatching.repository.routing.SnapPointToNodesDTO
+import fi.hsl.jore4.mapmatching.service.matching.PublicTransportRouteMatchingParameters.JunctionMatchingParameters
+import fi.hsl.jore4.mapmatching.util.LogUtils.joinToLogString
+import mu.KotlinLogging
+import org.geolatte.geom.G2D
+import org.geolatte.geom.Point
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+private val LOGGER = KotlinLogging.logger {}
+
+@Component
+class RoadJunctionMatcherImpl @Autowired constructor(val nodeRepository: INodeRepository) : IRoadJunctionMatcher {
+
+    @Transactional(readOnly = true, noRollbackFor = [RuntimeException::class])
+    override fun findInfrastructureNodesMatchingRoadJunctions(routePoints: List<RoutePoint>,
+                                                              vehicleType: VehicleType,
+                                                              matchingParameters: JunctionMatchingParameters)
+        : Map<Int, NodeProximity?> {
+
+        val matchDistance: Double = matchingParameters.junctionNodeMatchDistance
+        val clearingDistance: Double = matchingParameters.junctionNodeClearingDistance
+
+        val junctionPointsWithRoutePointOrdering: List<IndexedValue<RoutePoint>> = routePoints
+            .withIndex()
+            .filter { it.value is RouteJunctionPoint }
+
+        val fromJunctionPointOneBasedIndexToRoutePointIndex: Map<Int, Int> = junctionPointsWithRoutePointOrdering
+            .map(IndexedValue<*>::index)
+            .withIndex()
+            .associateBy(keySelector = { it.index + 1 }, valueTransform = IndexedValue<Int>::value)
+
+        val pointCoordinates: List<Point<G2D>> = junctionPointsWithRoutePointOrdering.map { it.value.location }
+
+        val nClosestNodes: Map<Int, SnapPointToNodesDTO> = nodeRepository.findNClosestNodes(pointCoordinates,
+                                                                                            vehicleType,
+                                                                                            clearingDistance)
+
+        return nClosestNodes.entries
+            .mapNotNull { (junctionPointOneBasedIndex: Int, snap: SnapPointToNodesDTO) ->
+
+                val routePointIndex: Int = fromJunctionPointOneBasedIndexToRoutePointIndex[junctionPointOneBasedIndex]!!
+
+                val nodes: List<NodeProximity> = snap.nodes
+
+                when (nodes.size) {
+                    1 -> {
+                        val node: NodeProximity = nodes[0]
+
+                        if (node.distanceToNode <= matchDistance)
+                            routePointIndex to node
+                        else
+                            null
+                    }
+
+                    else -> null
+                }
+            }
+            .also { routePointIndexToJunctionNode: List<Pair<Int, NodeProximity>> ->
+                LOGGER.debug {
+                    "Matched following road junction points from source route points: ${
+                        joinToLogString(routePointIndexToJunctionNode) {
+                            "Route point #${it.first + 1}: ${it.second}"
+                        }
+                    }"
+                }
+            }
+            .toMap()
+    }
+}
