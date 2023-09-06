56c3eabda536fa3fd3b063207edda4af7ebbc32c
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-09-06 15:35:36 +0300

    Fix use of deprecated WebSecurityConfigurerAdapter.

--- spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebSecurityConfig.kt
+++ spring-boot/src/main/kotlin/fi/hsl/jore4/mapmatching/config/WebSecurityConfig.kt
@@ -2,38 +2,46 @@ package fi.hsl.jore4.mapmatching.config
 
 import fi.hsl.jore4.mapmatching.api.MapMatchingController
 import fi.hsl.jore4.mapmatching.api.RouteController
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
 
-    override fun configure(httpSec: HttpSecurity) {
-        httpSec
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+    @Bean
+    @Throws(Exception::class)
+    fun configure(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.NEVER) }
 
-            .and()
+            // CSRF is not needed.
             .csrf().disable()
 
-            .authorizeRequests()
+            /** A CORS mapping is defined in [WebConfig] within "development" Spring profile. */
+            .cors(withDefaults())
 
-            .antMatchers(HttpMethod.GET,
-                         RouteController.URL_PREFIX + "/**",
-                         "/actuator/health",
-                         "/*" // matches static landing page for examining results from route API
-            ).permitAll()
+            .authorizeHttpRequests {
+                it
+                    .antMatchers(HttpMethod.GET,
+                                 RouteController.URL_PREFIX + "/**",
+                                 "/actuator/health",
+                                 "/*" // matches static landing page for examining results from route API
+                    ).permitAll()
 
-            .antMatchers(HttpMethod.POST,
-                         MapMatchingController.URL_PREFIX + "/**",
-                         RouteController.URL_PREFIX + "/**"
-            ).permitAll()
+                    .antMatchers(HttpMethod.POST,
+                                 MapMatchingController.URL_PREFIX + "/**",
+                                 RouteController.URL_PREFIX + "/**"
+                    ).permitAll()
 
-            .anyRequest().denyAll()
+                    .anyRequest().denyAll()
+            }
+            .build()
     }
 }
