857233f27e5a64b034cc9796fb8a69412cc4be11
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-20 12:31:42 +0200

    Fix a missing double quote in `build-jdbc-urls.sh`.

--- scripts/build-jdbc-urls.sh
+++ scripts/build-jdbc-urls.sh
@@ -6,6 +6,6 @@ set -eu
 
 # if DB_URL was not set, build it from parts
 if [ -z "${DB_URL+x}" ]; then
-  export DB_PORT=${DB_PORT:-"5432"}
+  export DB_PORT="${DB_PORT:-"5432"}"
   export DB_URL="jdbc:postgresql://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}?stringtype=unspecified"
 fi
